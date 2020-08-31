fe108c4ff130ee3d369be0e3c7cda19df45db6ec
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-31 22:29:01 +0100

    Upload metadata and file finder

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ fof = read.fof_groups(files, header)
 read.pprint(header.data.subfind_particles.MassTable)
 
 for key in fof.data.subfind_tab.FOF:
-    read.pprint(len(fof.data.subfind_tab.FOF[key]), fof.data.subfind_tab.FOF[key])
+    read.pprint(key, len(fof.data.subfind_tab.FOF[key]), fof.data.subfind_tab.FOF[key][:2])
 
 for key in fof.data.subfind_tab.Subhalo:
-    read.pprint(len(fof.data.subfind_tab.Subhalo[key]), fof.data.subfind_tab.Subhalo[key])
+    read.pprint(key, len(fof.data.subfind_tab.Subhalo[key]), fof.data.subfind_tab.Subhalo[key][:2])
+
+for key in fof.data.group_tab.FOF:
+    read.pprint(key, len(fof.data.group_tab.FOF[key]), fof.data.group_tab.FOF[key][:2])
