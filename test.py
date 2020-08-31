f293468d606a0bb8015ff3bc2943fff2e325c1fc
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-31 18:10:44 +0100

    Upload metadata and file finder

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import read
 
 files = read.find_files('hydro', 'z000p000')
-print(files[0], files[-1])
-print(files[0], files[-1])
-print(files[0], files[-1])
-print(files[0], files[-1])
+print(files[0][0], files[0][-1])
+print(files[1][0], files[1][-1])
+print(files[2][0], files[2][-1])
+print(files[3][0], files[3][-1])
