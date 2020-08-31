7c37662ead699a47d25212c570dea6d201828eed
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-31 21:46:38 +0100

    Upload metadata and file finder

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import read
 
 files = read.find_files('hydro', 'z000p000')
 header = read.get_header(files)
-fof = read.fof_groups(files)
+fof = read.fof_groups(files, header)
 read.pprint(header.data.subfind_particles.MassTable)
 read.pprint(len(fof.data.subfind_tab.FOF.Group_M_Crit200), fof.data.subfind_tab.FOF.Group_M_Crit200)
 read.pprint(len(fof.data.subfind_tab.FOF.GroupOffset), fof.data.subfind_tab.FOF.GroupOffset)
