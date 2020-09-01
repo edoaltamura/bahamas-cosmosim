de9b0d7600a0e87198bec7fc5265ecfeb0fc6ef6
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-01 20:49:50 +0100

    Offset metadata upload

--- offset_type.py
+++ offset_type.py
@@ -3,6 +3,12 @@
 
 import numpy as np
 import h5py as h5
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nproc = comm.Get_size()
+
 from read import find_files, split, commune, get_header, pprint
 
 simulation_type = 'hydro'
