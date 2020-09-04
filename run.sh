76f7bc6149dcdaa3bd64c395480b9828f69ae6a8
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-04 21:56:19 +0100

    Offset metadata upload

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 -u ./offset_type.py > ./main.log &
-/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 ./offset_type.py
+#/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 ./offset_type.py
+/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 ./test.py
