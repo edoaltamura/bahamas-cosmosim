8caa1bacb49ff2929438842b034d8c003985f6be
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-05 01:11:26 +0100

    Offset metadata upload

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ files = read.find_files('hydro', 'z003p000')
 header = read.get_header(files)
 fofs = read.fof_groups(files, header)
 read.pprint("Dark matter particle mass:", fofs.data.mass_DMpart)
+csrm = read.csr_index_matrix(files)
 
 # for key in fofs.data.subfind_tab.FOF:
 #     read.pprint(f"subfind_tab.FOF.{key:<30s} {len(fofs.data.subfind_tab.FOF[key])}", fofs.data.subfind_tab.FOF[key][:2])
@@ -24,7 +25,7 @@ fof = read.fof_group(0, fofs)
 # for key in fof.data.group_tab.FOF:
 #     read.pprint(f"group_tab.FOF.{key:<30s}", fof.data.group_tab.FOF[key])
 
-particles = read.fof_particles(fof)
+particles = read.fof_particles(fof, csrm)
 for pt in ['0', '1', '4']:
     # for key in particles.data.subfind_particles[f'PartType{pt}']:
     #     read.pprint(f"PartType{pt:s}.{key:<30s}", particles.data.subfind_particles[f'PartType{pt}'][key])
