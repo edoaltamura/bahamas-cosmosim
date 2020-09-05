0635b8d6b087a3317fee1c9d40e0d12729a7ba03
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-05 23:33:08 +0100

    Particle maps

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 -u ./offset_type.py > ./main.log &
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 ./offset_type.py
-/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 ./map_particles.py
+/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3 ./map_particles.py --gas --dark-matter --stars
