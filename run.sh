a3483a1b1611430b8183dc760659e6f6a70ed0f6
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-06 20:53:04 +0100

    Particle maps

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ OMP_NUM_THREADS=12
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 -u ./offset_type.py > ./main.log &
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 ./offset_type.py
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 ./map_particles.py --gas --dark_matter --stars
-/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 -u ./map_density.py --gas --dark_matter --stars > ./main.log &
+/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 ./map_density.py --gas --dark_matter --stars
