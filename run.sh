23a64c5f46cff0362a21e0f75ee3a5f87be4b915
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-07 23:05:14 +0100

    Particle maps

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+#Activate the virtual environment with Swiftsimio
+source /local/scratch/altamura/rksz-venv/bin/activate.csh
+
 OMP_NUM_THREADS=12
-NUMBA_NUM_THREADS=$OMP_NUM_THREADS
+NUMBA_NUM_THREADS=12
 
 export OMP_NUM_THREADS
 export NUMBA_NUM_THREADS
@@ -9,4 +12,4 @@ export NUMBA_NUM_THREADS
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 -u ./offset_type.py > ./main.log &
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 ./offset_type.py
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 ./map_particles.py --gas --dark_matter --stars
-/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 ./map_density.py --gas --stars
+/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n $OMP_NUM_THREADS python3 -u ./map_density.py --gas --stars > ./main.log &
