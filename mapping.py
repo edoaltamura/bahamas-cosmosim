2af1eb45fde8c90e3b3e68aa6578fa28b1f7e78d
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-10-09 18:56:54 +0100

    Pickle implement for fast testing

--- mapping.py
+++ mapping.py
@@ -473,6 +473,8 @@ class Mapping:
 
 
 if __name__ == '__main__':
+    import pickle
+    import os
     # -------------------------------------------------------------------- #
     # Edit these parameters
     simulation_type = 'hydro'
@@ -480,11 +482,17 @@ if __name__ == '__main__':
     cluster_id = 0
     output_directory = '/local/scratch/altamura/bahamas/maps'
     # -------------------------------------------------------------------- #
-    # Boot up the BAHAMAS data
-    files = read.find_files(simulation_type, redshift)
-    fofs = read.fof_groups(files)
-    csrm = read.csr_index_matrix(fofs)
-    fof = read.fof_group(cluster_id, fofs)
-    cluster_data = read.class_wrap(read.fof_particles(fof, csrm)).data
+    if not os.path.isfile(f'{output_directory}/test_cluster_data.pickle'):
+        # Boot up the BAHAMAS data
+        files = read.find_files(simulation_type, redshift)
+        fofs = read.fof_groups(files)
+        csrm = read.csr_index_matrix(fofs)
+        fof = read.fof_group(cluster_id, fofs)
+        cluster_data = read.class_wrap(read.fof_particles(fof, csrm)).data
+        with open(f'{output_directory}/test_cluster_data.pickle', 'wb') as handle:
+            pickle.dump(cluster_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open(f'{output_directory}/test_cluster_data.pickle', 'rb') as handle:
+        cluster_data = pickle.load(handle)
     # -------------------------------------------------------------------- #
     Mapping(cluster_data)
