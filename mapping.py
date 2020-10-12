df91cb14b0bd3a3e9181ccf922dd1829eb2f200c
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-10-12 23:10:47 +0100

    Upload profiles and properties.py and do tests on first subhalo Id

--- mapping.py
+++ mapping.py
@@ -516,33 +516,41 @@ if __name__ == '__main__':
     redshifts = ['z003p000', 'z002p000', 'z001p000', 'z000p250', 'z000p000']
     cluster_ids = [0, 1, 2, 50, 100, 200, 1000]
 
-    for redshift in redshifts:
+    # for redshift in redshifts:
+    #
+    #     # -------------------------------------------------------------------- #
+    #     # redshift = 'z003p000'
+    #     # cluster_id = 0
+    #     # -------------------------------------------------------------------- #
+    #
+    #     # Boot up the BAHAMAS data
+    #     files = read.find_files(simulation_type, redshift)
+    #     fofs = read.fof_groups(files)
+    #     csrm = read.csr_index_matrix(fofs)
+    #
+    #     for n in cluster_ids:
+    #
+    #         if not os.path.isfile(f'{output_directory}/test_cluster_data.{redshift}_{n}.pickle'):
+    #
+    #             fof = read.fof_group(n, fofs)
+    #             cluster_dict = read.fof_particles(fof, csrm)
+    #             read.pprint("Dark matter particle mass:", cluster_dict['mass_DMpart'])
+    #
+    #             with open(f'{output_directory}/test_cluster_data.{redshift}_{n}.pickle', 'wb') as handle:
+    #                 pickle.dump(cluster_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    #
+    #         if not os.path.isfile(f'{output_directory}/test.{redshift}_{n}.png'):
+    #
+    #             with open(f'{output_directory}/test_cluster_data.{redshift}_{n}.pickle', 'rb') as handle:
+    #                 cluster_dict = pickle.load(handle)
+    #
+    #             cluster_data = read.class_wrap(cluster_dict).data
+    #             Mapping(cluster_data)
 
-        # -------------------------------------------------------------------- #
-        # redshift = 'z003p000'
-        # cluster_id = 0
-        # -------------------------------------------------------------------- #
 
-        # Boot up the BAHAMAS data
-        files = read.find_files(simulation_type, redshift)
-        fofs = read.fof_groups(files)
-        csrm = read.csr_index_matrix(fofs)
-
-        for n in cluster_ids:
-
-            if not os.path.isfile(f'{output_directory}/test_cluster_data.{redshift}_{n}.pickle'):
-
-                fof = read.fof_group(n, fofs)
-                cluster_dict = read.fof_particles(fof, csrm)
-                read.pprint("Dark matter particle mass:", cluster_dict['mass_DMpart'])
-
-                with open(f'{output_directory}/test_cluster_data.{redshift}_{n}.pickle', 'wb') as handle:
-                    pickle.dump(cluster_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-            if not os.path.isfile(f'{output_directory}/test.{redshift}_{n}.png'):
-
-                with open(f'{output_directory}/test_cluster_data.{redshift}_{n}.pickle', 'rb') as handle:
-                    cluster_dict = pickle.load(handle)
-
-                cluster_data = read.class_wrap(cluster_dict).data
-                Mapping(cluster_data)
+    files = read.find_files(simulation_type, redshifts[0])
+    fofs = read.fof_groups(files)
+    csrm = read.csr_index_matrix(fofs)
+    fof = read.fof_group(cluster_ids[0], fofs)
+    cluster_dict = read.fof_particles(fof, csrm)
+    read.pprint("Dark matter particle mass:", cluster_dict['mass_DMpart'])
