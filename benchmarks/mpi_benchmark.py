5658cc2355f02e57b2512907537f46fcc3956d69
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-11-22 18:28:32 +0000

    Upload benchmark scripts

--- benchmarks/mpi_benchmark.py
+++ benchmarks/mpi_benchmark.py
@@ -44,26 +44,26 @@ for msg_length in [10, 1000, 10000, 100000, 1000000]:
         recvstop = datetime.datetime.now()
 
         # if a spawned node, report communication latencies in microseconds
-        startdelta = float((recvstart - startdata).microseconds)
-        stopdelta = float((recvstop - stopdata).microseconds)
-        transmitdelta = float((stopdata - startdata).microseconds)
+        startdelta = float((recvstart - startdata).milliseconds)
+        stopdelta = float((recvstop - stopdata).milliseconds)
+        transmitdelta = float((stopdata - startdata).milliseconds)
 
 
     startdelta_sum = comm.reduce(startdelta, op=MPI.SUM, root=0)
     stopdelta_sum = comm.reduce(stopdelta, op=MPI.SUM, root=0)
     transmitdelta_sum = comm.reduce(transmitdelta, op=MPI.SUM, root=0)
 
-    startdelta_min = comm.reduce(startdelta, op=MPI.MIN, root=0)
-    stopdelta_min = comm.reduce(stopdelta, op=MPI.MIN, root=0)
-    transmitdelta_min = comm.reduce(transmitdelta, op=MPI.MIN, root=0)
+    startdelta_min = comm.reduce(startdelta, op=MPI.MINLOC, root=0)
+    stopdelta_min = comm.reduce(stopdelta, op=MPI.MINLOC, root=0)
+    transmitdelta_min = comm.reduce(transmitdelta, op=MPI.MINLOC, root=0)
 
     startdelta_max = comm.reduce(startdelta, op=MPI.MAX, root=0)
     stopdelta_max = comm.reduce(stopdelta, op=MPI.MAX, root=0)
     transmitdelta_max = comm.reduce(transmitdelta, op=MPI.MAX, root=0)
 
     if rank == 0:
-        print(f'start difference (usec) : {startdelta_sum / (size - 1):.0f} | min {startdelta_min:.0f} | max {startdelta_max:.0f} ')
-        print(f'stop difference (usec) : {stopdelta_sum / (size - 1):.0f} | min {stopdelta_min:.0f} | max {stopdelta_max:.0f} ')
-        print(f'transmit difference (usec) : {transmitdelta_sum / (size - 1):.0f} | min {transmitdelta_min:.0f} | max {transmitdelta_max:.0f} ')
+        print(f'start difference (msec) : {startdelta_sum / (size - 1):.0f} | min {startdelta_min:.0f} | max {startdelta_max:.0f} ')
+        print(f'stop difference (msec) : {stopdelta_sum / (size - 1):.0f} | min {stopdelta_min:.0f} | max {stopdelta_max:.0f} ')
+        print(f'transmit difference (msec) : {transmitdelta_sum / (size - 1):.0f} | min {transmitdelta_min:.0f} | max {transmitdelta_max:.0f} ')
 
     comm.Barrier()  # wait for all hosts
