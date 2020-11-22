631edb23cb4a7cef178bc244bf9fad52399c6869
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-11-22 17:28:11 +0000

    Upload benchmark scripts

--- benchmarks/mpi_benchmark.py
+++ benchmarks/mpi_benchmark.py
@@ -1,27 +1,37 @@
 from mpi4py import MPI
 import datetime
+import numpy as np
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
 
-if rank == 0:  # launching process
-    start = datetime.datetime.now()
-    comm.send(start, dest=1, tag=11)
-    stop = datetime.datetime.now()
-    comm.send(stop, dest=1, tag=12)
-elif rank == 1:  # spawned processes
-    startdata = comm.recv(source=0, tag=11)
+# master process
+if rank == 0:
+    data = np.ones(1000000)
+    # master process sends data to worker processes by
+    # going through the ranks of all worker processes
+    for i in range(1, size):
+        comm.send(datetime.datetime.now(), dest=i, tag=i)
+        comm.send(data, dest=i, tag=i * 2)
+        comm.send(datetime.datetime.now(), dest=i, tag=i * 3)
+
+# worker processes
+else:
+    # each worker process receives data from master process
+    startdata = comm.recv(source=0, tag=rank)
     recvstart = datetime.datetime.now()
-    stopdata = comm.recv(source=0, tag=12)
+    data = comm.recv(source=0, tag=rank * 2)
+    stopdata = comm.recv(source=0, tag=rank * 3)
     recvstop = datetime.datetime.now()
 
-comm.Barrier()  # wait for all hosts
-
-# if a spawned node, report communication latencies in microseconds
-if rank == 1:
+    # if a spawned node, report communication latencies in microseconds
+    print(f"Rank {rank}:")
     startdelta = recvstart - startdata
-    print('start difference (uS) : ' + str(startdelta.microseconds) + '\n')
+    print('start difference (uS) : ' + str(startdelta.microseconds))
     stopdelta = recvstop - stopdata
-    print('stop difference (uS) : ' + str(stopdelta.microseconds) + '\n')
+    print('stop difference (uS) : ' + str(stopdelta.microseconds))
     transmitdelta = stopdata - startdata
     print('transmit difference (uS) : ' + str(transmitdelta.microseconds) + '\n')
+
+comm.Barrier()  # wait for all hosts
