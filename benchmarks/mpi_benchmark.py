931d25851b1465d0e0adc277d5ffe3ef10a6690c
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-11-22 18:02:10 +0000

    Upload benchmark scripts

--- benchmarks/mpi_benchmark.py
+++ benchmarks/mpi_benchmark.py
@@ -6,49 +6,52 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-# master process
-if rank == 0:
-    data = np.ones(1000000)
-    startdelta = 0.
-    stopdelta = 0.
-    transmitdelta = 0.
-    # master process sends data to worker processes by
-    # going through the ranks of all worker processes
-    for i in range(1, size):
-        comm.send(datetime.datetime.now(), dest=i, tag=i)
-        comm.send(data, dest=i, tag=i * 2)
-        comm.send(datetime.datetime.now(), dest=i, tag=i * 3)
+for msg_length in [10, 1000, 10000, 100000, 1000000]:
 
-# worker processes
-else:
-    # each worker process receives data from master process
-    startdata = comm.recv(source=0, tag=rank)
-    recvstart = datetime.datetime.now()
-    data = comm.recv(source=0, tag=rank * 2)
-    stopdata = comm.recv(source=0, tag=rank * 3)
-    recvstop = datetime.datetime.now()
+    # master process
+    if rank == 0:
+        data = np.ones(msg_length)
+        print(f"Message size: {data.nbytes}")
+        startdelta = 0.
+        stopdelta = 0.
+        transmitdelta = 0.
+        # master process sends data to worker processes by
+        # going through the ranks of all worker processes
+        for i in range(1, size):
+            comm.send(datetime.datetime.now(), dest=i, tag=i)
+            comm.send(data, dest=i, tag=i * 2)
+            comm.send(datetime.datetime.now(), dest=i, tag=i * 3)
 
-    # if a spawned node, report communication latencies in microseconds
-    startdelta = float((recvstart - startdata).microseconds)
-    stopdelta = float((recvstop - stopdata).microseconds)
-    transmitdelta = float((stopdata - startdata).microseconds)
+    # worker processes
+    else:
+        # each worker process receives data from master process
+        startdata = comm.recv(source=0, tag=rank)
+        recvstart = datetime.datetime.now()
+        data = comm.recv(source=0, tag=rank * 2)
+        stopdata = comm.recv(source=0, tag=rank * 3)
+        recvstop = datetime.datetime.now()
+
+        # if a spawned node, report communication latencies in microseconds
+        startdelta = float((recvstart - startdata).microseconds)
+        stopdelta = float((recvstop - stopdata).microseconds)
+        transmitdelta = float((stopdata - startdata).microseconds)
 
 
-startdelta = comm.reduce(startdelta, op=MPI.SUM, root=0) / (size - 1)
-stopdelta = comm.reduce(stopdelta, op=MPI.SUM, root=0) / (size - 1)
-transmitdelta = comm.reduce(transmitdelta, op=MPI.SUM, root=0) / (size - 1)
+    startdelta = comm.reduce(startdelta, op=MPI.SUM, root=0) / (size - 1)
+    stopdelta = comm.reduce(stopdelta, op=MPI.SUM, root=0) / (size - 1)
+    transmitdelta = comm.reduce(transmitdelta, op=MPI.SUM, root=0) / (size - 1)
 
-startdelta_min = comm.reduce(startdelta, op=MPI.MIN, root=0)
-stopdelta_min = comm.reduce(stopdelta, op=MPI.MIN, root=0)
-transmitdelta_min = comm.reduce(transmitdelta, op=MPI.MIN, root=0)
+    startdelta_min = comm.reduce(startdelta, op=MPI.MIN, root=0)
+    stopdelta_min = comm.reduce(stopdelta, op=MPI.MIN, root=0)
+    transmitdelta_min = comm.reduce(transmitdelta, op=MPI.MIN, root=0)
 
-startdelta_max = comm.reduce(startdelta, op=MPI.MAX, root=0)
-stopdelta_max = comm.reduce(stopdelta, op=MPI.MAX, root=0)
-transmitdelta_max = comm.reduce(transmitdelta, op=MPI.MAX, root=0)
+    startdelta_max = comm.reduce(startdelta, op=MPI.MAX, root=0)
+    stopdelta_max = comm.reduce(stopdelta, op=MPI.MAX, root=0)
+    transmitdelta_max = comm.reduce(transmitdelta, op=MPI.MAX, root=0)
 
-if rank == 0:
-    print(f'start difference (usec) : {startdelta:.0f} | min {startdelta_min:.0f} | max {startdelta_max:.0f} ')
-    print(f'stop difference (usec) : {stopdelta:.0f} | min {stopdelta_min:.0f} | max {stopdelta_max:.0f} ')
-    print(f'transmit difference (usec) : {transmitdelta:.0f} | min {transmitdelta_min:.0f} | max {transmitdelta_max:.0f} ')
+    if rank == 0:
+        print(f'start difference (usec) : {startdelta:.0f} | min {startdelta_min:.0f} | max {startdelta_max:.0f} ')
+        print(f'stop difference (usec) : {stopdelta:.0f} | min {stopdelta_min:.0f} | max {stopdelta_max:.0f} ')
+        print(f'transmit difference (usec) : {transmitdelta:.0f} | min {transmitdelta_min:.0f} | max {transmitdelta_max:.0f} ')
 
-comm.Barrier()  # wait for all hosts
+    comm.Barrier()  # wait for all hosts
