c847ea1b0d382868289dd108e04f2dcbcce76ae2
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-17 23:46:26 +0200

    Upload benchmark scripts

--- visualise_box.py
+++ visualise_box.py
@@ -17,35 +17,36 @@ files = read.find_files(simulation_type, redshift)
 # halo_catalogue = read.fof_groups(files)
 snapshot = read.snapshot_data(files)['snaps']
 
-x_max = np.max(snapshot['PartType0']['Coordinates'][:, 0])
-x_min = np.min(snapshot['PartType0']['Coordinates'][:, 0])
-y_max = np.max(snapshot['PartType0']['Coordinates'][:, 1])
-y_min = np.min(snapshot['PartType0']['Coordinates'][:, 1])
-x_range = x_max - x_min
-y_range = y_max - y_min
-x = (snapshot['PartType0']['Coordinates'][:, 0] - x_min) / x_range
-y = (snapshot['PartType0']['Coordinates'][:, 1] - y_min) / y_range
-h = snapshot['PartType0']['SmoothingLength'] / x_range
+if read.rank == 0:
+    x_max = np.max(snapshot['PartType0']['Coordinates'][:, 0])
+    x_min = np.min(snapshot['PartType0']['Coordinates'][:, 0])
+    y_max = np.max(snapshot['PartType0']['Coordinates'][:, 1])
+    y_min = np.min(snapshot['PartType0']['Coordinates'][:, 1])
+    x_range = x_max - x_min
+    y_range = y_max - y_min
+    x = (snapshot['PartType0']['Coordinates'][:, 0] - x_min) / x_range
+    y = (snapshot['PartType0']['Coordinates'][:, 1] - y_min) / y_range
+    h = snapshot['PartType0']['SmoothingLength'] / x_range
 
-# Gather and handle coordinates to be processed
-x = np.asarray(x.value, dtype=np.float64)
-y = np.asarray(y.value, dtype=np.float64)
-m = np.asarray(snapshot['PartType0']['Density'].value, dtype=np.float32)
-h = np.asarray(h.value, dtype=np.float32)
-read.pprint(f'Computing map ({resolution} x {resolution})')
-smoothed_map = scatter(x=x, y=y, m=m, h=h, res=resolution).T
-smoothed_map = np.ma.masked_where(np.abs(smoothed_map) < 1.e-9, smoothed_map)
+    # Gather and handle coordinates to be processed
+    x = np.asarray(x.value, dtype=np.float64)
+    y = np.asarray(y.value, dtype=np.float64)
+    m = np.asarray(snapshot['PartType0']['Density'].value, dtype=np.float32)
+    h = np.asarray(h.value, dtype=np.float32)
+    read.pprint(f'Computing map ({resolution} x {resolution})')
+    smoothed_map = scatter(x=x, y=y, m=m, h=h, res=resolution).T
+    smoothed_map = np.ma.masked_where(np.abs(smoothed_map) < 1.e-9, smoothed_map)
 
-fig, axes = plt.subplots()
-cmap = copy.copy(plt.get_cmap('twilight'))
-cmap.set_under('black')
-axes.axis("off")
-axes.set_aspect("equal")
-axes.imshow(
-    smoothed_map.T,
-    norm=LogNorm(),
-    cmap=cmap,
-    origin="lower",
-    extent=[x_min, x_max, y_min, y_max]
-)
-plt.show()
+    fig, axes = plt.subplots()
+    cmap = copy.copy(plt.get_cmap('twilight'))
+    cmap.set_under('black')
+    axes.axis("off")
+    axes.set_aspect("equal")
+    axes.imshow(
+        smoothed_map.T,
+        norm=LogNorm(),
+        cmap=cmap,
+        origin="lower",
+        extent=[x_min, x_max, y_min, y_max]
+    )
+    plt.show()
